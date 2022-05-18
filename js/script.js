1f3dae14e652552b915c6cc25c35ca8c7f4cc92e
Volodymyr <vlodkograba@gmail.com>
2022-05-18 05:05:11 +0200

    film rate

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = prompt("How many films did you see?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("One of the last seen film", ""),
+  b = prompt("rate of this film", ""),
+  c = prompt("One of the last seen film", ""),
+  d = prompt("rate of this film", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
